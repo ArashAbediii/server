cdc724dc945fb600faae4764aa9f0c91b63c88db
Arash <arashabedi998@gmail.com>
2021-02-13 17:01:32 +0330

    v001

--- test.php
+++ test.php
@@ -9,6 +9,10 @@ $response=Server::sendRequest($url,$params,'get',[]);
 
 
 echo '<br><br>**** MADE BY ARASH ABEDI ****<br><br>GET REQUEST';
+
+//DEEZER
 echo '<pre>';
 var_dump(json_decode($response,true));
 echo '</pre>';
+
+//dev pack
